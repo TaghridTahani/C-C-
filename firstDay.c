d1a83954f1aff245574ee3b0a3b338dd46d7e577
Taghrid Tahani Preeti <94818949+TaghridTahani@users.noreply.github.com>
2023-10-02 00:04:34 +0600

    Add files via upload

---
+++ firstDay.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int a, b, sum ; 
+    printf("enter the first number: \n");
+    scanf("%d", &a);
+    printf("Enter the second value \n");
+    scanf("%d", &b);
+    printf("your first number is %d \n", a);
+    printf("your second number is %d \n", b);
+
+    printf("sum of a and b is %d\n", a+b);
+
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int a, b; 
+    scanf("%d",&a);
+    printf("%d",a);
+    printf("Hello \n  World");
+    return 0 ;
+} 
